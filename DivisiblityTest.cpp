41eb84110e1df52753a850efb327d04c70629f80
Siddhant-2029 <217794708+Siddhant-2029@users.noreply.github.com>
2025-06-26 14:41:28 +0530

    Add files via upload

---
+++ DivisiblityTest.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"this program check divisibility with 5\n";
+    cout<<"give your number\n";
+    int a;
+    cin>>a;
+    int y=a%5;
+    if(y==0)
+         cout<<"number is divisible by 5";
+    else
+        cout<<"not divisible by 5";
+    
+}
+
+
+
+
+
+
+
+
+
